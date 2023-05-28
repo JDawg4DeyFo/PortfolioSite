14c07e2b14c1b62a83741dca78d7d3c9ebda5123
Jahseh Dennon <jdennon@ucsc.edu>
2023-05-27 23:42:12 -0700

    started work again... don't know why I stopped for a lil

---
+++ scripts/calculator.js
@@ -0,0 +1,11 @@
+(function(){
+    // This function will initialize everything.
+   })();
+
+/*
+TO DO:
+clone html elements to add courses on button click
+get values from html elements
+calculate on button click
+integrate base stats
+*/

ef383c5ba9e170d04ae6da4113435492719422ba
JDawg <jacob.dennon@gmail.com>
2023-07-03 22:06:57 -0700

    i guess the change in my pocket ust wanst enough

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -2,18 +2,26 @@ const dateMeta = document.querySelector('meta[name="date"]');
 const date = dateMeta.getAttribute('content');
 console.log(date);
 
+let PostArray = [];
+
+(function() {
+  // Attach Event Listeners to random button and tag sort thing
 
 
-  (function() {
-    fetch('../pages/posts/.html')
-    .then(response => response.text())
-    .then(data => {
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(data, 'text/html');
-    const title = htmlDocument.querySelector('title').textContent;
-    
-    // Do something with the retrieved data
-    console.log(title); 
-  })
-    .catch(error => console.error(error));
-  })();
+  // get da DOMS
+  fetch('../pages/posts/.html')
+  .then(response => response.text())
+  .then(data => {
+  const parser = new DOMParser();
+  const htmlDocument = parser.parseFromString(data, 'text/html');
+  const title = htmlDocument.querySelector('title').textContent;
+  
+  // Do something with the retrieved data
+  console.log(title); 
+})
+  .catch(error => console.error(error));
+})();
+
+function GoToRandomPost(event) {
+
+}

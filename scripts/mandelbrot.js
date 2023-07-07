2243b642d9fe44e63277ab48d5e9cd6b69a2bbbc
JDawg <jacob.dennon@gmail.com>
2023-07-07 12:58:14 -0700

    more work on mandelbrot set stuff

--- scripts/mandelbrot.js
+++ scripts/mandelbrot.js
@@ -1,42 +1,88 @@
-(function() {
-    // Initialize stuff
-    const DotTemplate = document.querySelector('#dot-template');
-    const BreakLineTemplate = document.createElement('br');
+let DotMatrix = [];
 
-    const DotWidth = DotTemplate.offsetWidth;
-    const DotHeight = DotTemplate.offsetHeight;
+function MandelbrotAlgorithm(TopLeft, BottomRight, Zoom) {
+  /*
+  for each pixel (Px, Py) on the screen do
+      x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
+      y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
+      x := 0.0
+      y := 0.0
+      iteration := 0
+      max_iteration := 1000
+      while (x*x + y*y ≤ 2*2 AND iteration < max_iteration) do
+          xtemp := x*x - y*y + x0
+          y := 2*x*y + y0
+          x := xtemp
+          iteration := iteration + 1
 
-    const ViewportWidth = window.innerWidth;
-    const ViewportHeight = window.innerHeight;
-    const AspectRatio = ViewportWidth / ViewportHeight;
+      color := palette[iteration]
+      plot(Px, Py, color)
+  */
+  const MaxIteration = 1000;
 
-    // populate screen with apropriate rows and cols of dots.
-    console.log(ViewportWidth, DotWidth);
-    const Cols = Math.floor(ViewportWidth / DotWidth);
-    const Rows = Math.floor(ViewportHeight / DotHeight);
-    console.log(Rows, Cols);
 
-    // dot matrix
-    let DotMatrix = [];
+  DotMatrix.forEach(function (Item, Index) {
+    Item.forEach(function (NestedItem, NestedIndex) {
 
-    const DotSavior = document.createElement('span');
-    DotSavior.textContent = '.';
+    });
+  });
+}
 
-    fragment = document.createDocumentFragment();
+function ZoomIn(MouseX, MouseY) {
+  console.log('zoomed in', { MouseX, MouseY });
+  /*
+    need to set up new coords for each dotxel on zoom in
+    joe comon black top she got ju ju eyeballs!
+  */
+}
 
-    for (let i = 0; i < Rows; i++) {
-        DotMatrix[i] = [];
+(function () {
+  // Initialize stuff
+  const DotTemplate = document.querySelector('#dot-template');
+  const BreakLineTemplate = document.createElement('br');
+  const DivTemplate = document.createElement('div');
 
-        for (let j = 0; j < Cols; j++) {
-            const NewDot = DotSavior.cloneNode(true);
+  const DotWidth = DotTemplate.offsetWidth;
+  const DotHeight = DotTemplate.offsetHeight;
+  const ViewportWidth = document.body.clientWidth;
+  let ViewportHeight = window.innerHeight;
+  const AspectRatio = ViewportWidth / ViewportHeight;
 
-            DotMatrix[i][j] = NewDot;
+  DotTemplate.style.display = 'none';
 
-            fragment.appendChild(NewDot);
-        }
-        
-        fragment.appendChild(BreakLineTemplate.cloneNode(false));
+  // populate screen with apropriate rows and cols of dots.
+  console.log({ DotWidth, DotHeight, ViewportWidth, ViewportHeight });
+  const Cols = 200;
+  const Rows = 200;
+  console.log({ Rows, Cols });
+
+  const DotSavior = document.createElement('span');
+  DotSavior.textContent = '.';
+
+  fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < Rows; i++) {
+    DotMatrix[i] = [];
+
+    NewDiv = DivTemplate.cloneNode(false);
+    fragment.append(NewDiv);
+
+    for (let j = 0; j < Cols; j++) {
+      const NewDot = DotSavior.cloneNode(true);
+
+      DotMatrix[i][j] = NewDot;
+
+      NewDiv.appendChild(NewDot);
     }
+  }
 
-    document.body.appendChild(fragment);
+  document.body.appendChild(fragment);
+
+  // Add Event Listeners
+  window.addEventListener('scroll', function (event) {
+    const MouseX = event.ClientX;
+    const MouseY = event.ClinetY;
+
+    ZoomIn(MouseX, MouseY);
+  })
 })();
